1bfa00f6af7bfffe76e8c268c7d40776d60d3a32
namiriii <nik43@naver.com>
2026-03-22 05:10:53 +0900

    feat: controller-service 구조 분리

--- src/main/java/com/namil/springbootstudy/controller/HelloController.java
+++ src/main/java/com/namil/springbootstudy/controller/HelloController.java
@@ -1,6 +1,7 @@
 package com.namil.springbootstudy.controller;
 import com.namil.springbootstudy.dto.HelloRequest;
 import com.namil.springbootstudy.dto.HelloResponse;
+import com.namil.springbootstudy.service.HelloService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,6 +12,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
+    private final HelloService helloService;
+
+    public HelloController(HelloService helloService) {
+        this.helloService = helloService;
+    }
+
     @GetMapping("/hello")
     public HelloResponse hello() {
         return new HelloResponse("hello spring boot", "10:00");
@@ -18,7 +25,7 @@ public class HelloController {
 
     @PostMapping("/hello")
     public HelloResponse helloPost(@RequestBody HelloRequest request) {
-        return new HelloResponse(request.getName(), "응답시간");
+        return helloService.hello(request);
     }
 
 
